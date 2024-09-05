7c7c9ecfd611254c354aafa7c8f7b0274806e1bb
MadhuriMaadhu <123137841+MadhuriMaadhu@users.noreply.github.com>
2024-09-05 23:28:27 +0530

    Update movieRoutes.js

--- Server/routes/v1/movieRoutes.js
+++ Server/routes/v1/movieRoutes.js
@@ -1,13 +1,14 @@
-import upload from "../../middlewares/multer";
-import adminAuth from "../../middlewares/adminAuth";
-import { createMovie, updateMovie, deleteMovie, getMovies, getMovieDetails} from "../../controllers/movieController";
-
+import express from 'express';
+import upload from "../../middlewares/multer.js";
+import adminAuth from "../../middlewares/adminAuth.js";
+import { createMovie, updateMovie, deleteMovie, getMovies, getMovieDetails} from "../../controllers/movieController.js";
 
 const router = express.Router();
+
 router.get("/movie-list", getMovies);
 router.get("/details/:movieId", getMovieDetails);
 router.post("/create", adminAuth, upload.single("image"), createMovie);
 router.put("/update/:movieId", adminAuth, upload.single("image"), updateMovie);
 router.delete("/delete/:movieId", adminAuth, deleteMovie);
 
-module.exports = { movieRouter: router };
+export default router;
